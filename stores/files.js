8a5b88c3f13f74dbe7e3f5582c24cdd6fad2f7a3
Ruben Paz <me@ruben.io>
2018-05-11 15:23:03 +0100

    Update file store

--- stores/files.js
+++ stores/files.js
@@ -1,34 +1,37 @@
 module.exports = store
 
 function store (state, emitter) {
-
   state.statuses = {
     FRESH: 'files::state_fresh',
     FILES_READY: 'files::state_files_ready'
   }
 
-  state.events.FILES_LOADED = 'files::loaded'
-  state.events.FILES_REQUESTED = 'files::requested'
+  state.events.FILES_LOADED = 'files:loaded'
+  state.events.FILES_REQUESTED = 'files:requested'
 
   state.files = []
   state.status = state.statuses.STATE_FRESH
-  state.archive = new DatArchive(window.location.href)
 
-
-  emitter.on(state.events.FILES_LOADED, (files) => {
-    state.files = files
-    state.status = state.statuses.FILES_READY
-    emitter.emit(state.events.RENDER)
-  })
-
-  emitter.on(state.events.FILES_REQUESTED, (dir = './') => {
-    state.archive.readdir(dir).then(files => {
-      return Promise.all(files.map(fileName => {
-        return state.archive.stat(fileName).then(result => ({...result, name: fileName}))
-      }))
+  emitter.on('DOMContentLoaded', function () {
+    emitter.on(state.events.FILES_LOADED, (files) => {
+      state.files = files
+      state.status = state.statuses.FILES_READY
+      emitter.emit(state.events.RENDER)
     })
-      .then(files => {
-        emitter.emit(state.events.FILES_LOADED, files)
+
+    emitter.on(state.events.ARCHIVE_READY, archive => {
+      emitter.emit(state.events.FILES_REQUESTED)
+    })
+
+    emitter.on(state.events.FILES_REQUESTED, (dir = './') => {
+      state.archive.readdir(dir).then(files => {
+        return Promise.all(files.map(fileName => {
+          return state.archive.stat(fileName).then(result => ({...result, name: fileName}))
+        }))
       })
+        .then(files => {
+          emitter.emit(state.events.FILES_LOADED, files)
+        })
+    })
   })
 }
